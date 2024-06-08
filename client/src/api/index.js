d3ed1e1e4e13a7675bf6c79447359b01081b2595
Abhrajit Das <abhrajitdas12245@gmail.com>
2024-06-08 14:16:08 +0530

    updated backend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: "http://localhost:8081/api/",
+  baseURL: "https://imageai-generator-s9ma.onrender.com/api/",
 });
 
 export const GetPosts = async () => await API.get("/post/");
